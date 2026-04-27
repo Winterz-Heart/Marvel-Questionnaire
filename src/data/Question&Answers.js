74410476ac4e05a8029740b1bddd592b526676ae
Winterz-Heart <duncan47685@gmail.com>
2026-04-27 15:58:56 +0100

    Added more questions

--- src/data/Question&Answers.js
+++ src/data/Question&Answers.js
@@ -1,12 +1,102 @@
 export const questions = [
     {
-        question: 'Who is Iron Man?',
-        answers: ['Steve Rogers', 'Bruce Banner', 'Peter Parker', 'Tony Stark'],
-        correctAnswer: 'Tony Stark',
+        question: 'How many Infinity Stones are there?',
+        answers: ['Three', 'Four', 'Five', 'Six'],
+        correctAnswer: 'Six',
     },
     {
-        question: 'How many Infinity Stones are there?',
-        answers: ['3', '4', '5', '6'],
-        correctAnswer: '6',
-    }
+        question: "Where is Captain America from?",
+        answers: ["Brooklyn", "Manhattan", "Staten Island", "Queens"],
+        correctAnswer: "Brooklyn",
+    },
+    {
+        question: "Who is Tony Stark's Farther?",
+        answers: ["Howard Stark", "Hank Stark", "Frank Stark", "Richard Stark"],
+        correctAnswer: "Howard Stark",
+    },
+    {
+        question: "What is Sharon Carter's relation to Peggy Carter?",
+        answers: ["Great Niece", "Grand Daughter", "Great Grand Daughter", "Great Grand Niece"],
+        correctAnswer: "Great Niece",
+    },
+    {
+        question: "What type of doctor was Doctor Strange?",
+        answers: ["Neurosurgeon", "Pediatrician", "Cardiosurgeon", "Dermatologist"],
+        correctAnswer: "Neurosurgeon",
+    },
+    {
+        question: "What is Captian America's shield made of?",
+        answers: ["Vibranium", "Adamantium", "Duranium", "Promethium"],
+        correctAnswer: "Vibranium",
+    },
+    {
+        question: "Who was able to pick up Thor's hammer in endgame?",
+        answers: ["Captain America", "Iron Man", "Spider-Man", "Rocket Racoon"],
+        correctAnswer: "Captain America",
+    },
+    {
+        question: "Who was responsible for King T'Chaka's death?",
+        answers: ["Zemo", "Baron von Strucker", "Alexander Pierce", "The Winter Soilder"],
+        correctAnswer: "Zemo",
+    },
+    {
+        question: "In which movie did Spider-Man first appear in the Marvel Cinematic Universe?",
+        answers: ["Captain America: Civil War", "Spider-Man: Homecoming", "Iron-Man 3", "Avengers: Infinity War"],
+        correctAnswer: "Captain America: Civil War",
+    },
+    {
+        question: "Wanda and her brother Pietro are from Where?",
+        answers: ["Sokovia", "Bolivia", "Czechoslovakia", "Russia"],
+        correctAnswer: "Sokovia",
+    },
+    {
+        question: "On which planet is the Soul Stone found?",
+        answers: ["Vormir", "Xander", "Titan", "Sakaar"],
+        correctAnswer: "Vormir",
+    },
+    {
+        question: "What movie started the Marvel Cinematic Universe?",
+        answers: ["Iron Man", "Captain America: The First Avenger", "Thor", "The Avengers"],
+        correctAnswer: "Iron Man",
+    },
+    {
+        question: "Who rescued Tony Stark and Nebula from Space?",
+        answers: ["Captain Marvel", "Thor", "Star Lord", "Shang-Chi"],
+        correctAnswer: "Captain Marvel",
+    },
+    {
+        question: "What is Pepper Potts allergic to?",
+        answers: ["Strawberries", "Cherries", "Blackberries", "Raspberries"],
+        correctAnswer: "Strawberiies",
+    },
+    {
+        question: "How many children does Hawkeye have?",
+        answers: ["Three", "Four", "Two", "One"],
+        correctAnswer: "Three",
+    },
+    {
+        question: "What is the name of Tony Stark's daughter?",
+        answers: ["Morgan", "Emma", "Maria", "Jane"],
+        correctAnswer: "Morgan",
+    },
+    {
+        question: "Star Lord/Peter Quill's farther Ego is a what?",
+        answers: ["Celestial", "Planet", "Genius loci", "World Mind"],
+        correctAnswer: "Celestial",
+    },
+    {
+        question: "What game is Thor playing in Avengers: Endgame?",
+        answers: ["Fortnite", "Apex: Legends", "Call of Duty: Warzone", "PUBG"],
+        correctAnswer: "Fortnite",
+    },
+    {
+        question: "How long was Scott lang in the Quantum Realm?",
+        answers: ["Five years", "Six weeks", "Eight months", "Three days"],
+        correctAnswer: "Five years",
+    },
+    {
+        question: "What are the names of Wanda's Twins?",
+        answers: ["Billy and Tommy", "Sam and Dean", "Fred and George", "Oliver and James"],
+        correctAnswer: "Billy and Tommy",
+    },
 ]
